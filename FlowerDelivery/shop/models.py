ec4f2fd2dd5a368b4acf202b96fc1411caeeeebf
Анатолий <lan2002@yandex.ru>
2024-08-29 04:44:34 +0300

    (update) FlowerDelivery\urls.py

--- FlowerDelivery/shop/models.py
+++ FlowerDelivery/shop/models.py
@@ -18,8 +18,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category',
-                       args=[self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -49,5 +48,4 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail',
-                       args=[self.id, self.slug])
+        return reverse('shop:product_detail', args=[self.id, self.slug])
