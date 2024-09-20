d8d88ea6bd927a729d74da5351e26a21bbb221bb
Анатолий <lan2002@yandex.ru>
2024-09-20 05:52:10 +0300

    (update) registration final

--- FlowerDelivery/account/forms.py
+++ FlowerDelivery/account/forms.py
@@ -22,6 +22,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
@@ -30,8 +36,7 @@ class UserEditForm(forms.ModelForm):
 
     def clean_email(self):
         data = self.cleaned_data['email']
-        qs = User.objects.exclude(id=self.instance.id) \
-            .filter(email=data)
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
         if qs.exists():
             raise forms.ValidationError('Email already in use.')
         return data
